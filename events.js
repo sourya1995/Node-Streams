9c88cbaf6860733a8f36033dd771d03de4264530
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-07-30 13:28:11 +0530

    basic event emitter

---
+++ events.js
@@ -0,0 +1,52 @@
+events = {
+  eventName1: [cb1, cb2],
+  eventName2: [cb3, cb4, cb5],
+  eventName3: [cb6]
+};
+
+class EventEmitter {
+    constructor(){
+        this._events = {};
+    }
+
+    addListener(eventName, listener){
+        const events = this._events;
+        if(!events[eventName])
+            events[eventName] = [];
+        events[eventName].push(listener);
+        return this;
+
+    }
+
+    on(eventName, listener){
+        return this.addListener(eventName, listener);
+    }
+
+    emit(eventName, ...args){
+        const events = this._events;
+        if(events[eventName]){
+            const listeners = events[eventName];
+            for(let i = 0; i < listeners.length; i++){
+                listeners[i].apply(this, args);
+            }
+        }else {
+            return false;
+        }
+        return true;
+    }
+
+    removeListener(eventName, listener){
+        const events = this._events;
+        if(events[eventName]){
+            for(let i = events[eventName].length - 1; i >= 0; i--){
+                if(events[eventName][i] === listener){
+                    events[eventName].splice(i, 1);
+                    break;
+                }
+            }
+            if(events[eventName].length === 0)
+                delete events[eventName];
+        }
+        return this;
+    }
+}
